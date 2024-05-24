90aae4f851e3760a0573cea5a15d04cbc766db51
Rocio Decuzzi Fernandez <rocio@rociodf.com>
2024-05-24 17:44:01 +0100

    add project

---
+++ src/cuentas/CCuenta.java
@@ -0,0 +1,50 @@
+package cuentas;
+
+public class CCuenta {
+    String nombre;
+    String cuenta;
+    double saldo;
+    double tipoInterés;
+
+    public CCuenta() {
+    }
+
+    public CCuenta(String nom, String cue, double sal, double tipo) {
+        nombre = nom;
+        cuenta = cue;
+        saldo = sal;
+    }
+
+    public double estado() {
+        return saldo;
+    }
+
+    public void retirar(double cantidad) {
+        try {
+            System.out.println("Retiro en cuenta:" + cantidad);
+            if (cantidad <= 0) {
+                throw new Exception("No se puede retirar una cantidad negativa");
+            }
+            if (estado() < cantidad) {
+                throw new Exception("No se hay suficiente saldo");
+            }
+            saldo = saldo - cantidad;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void ingresar(double cantidad) {
+        try {
+            System.out.println("Ingreso en cuenta:" + cantidad);
+
+            if (cantidad < 0) {
+                throw new Exception("No se puede ingresar una cantidad negativa");
+            }
+            saldo = saldo + cantidad;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
---
+++ src/cuentas/Main.java
@@ -0,0 +1,20 @@
+package cuentas;
+
+import cuentas.CCuenta;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es " + saldoActual);
+        // Operaciones de ingreso y retiro
+        cuenta1.retirar(2300);
+        cuenta1.ingresar(685);
+
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es " + saldoActual);
+    }    
+}
