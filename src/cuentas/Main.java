a4cadeb3d3dd72784a799f3e2d1c7c3c01b0f6bb
Rocio Decuzzi Fernandez <rocio@rociodf.com>
2024-05-24 17:46:24 +0100

    refactorizado método operaciones

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -10,10 +10,14 @@ public class Main {
         cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es " + saldoActual);
+        operaciones(cuenta1);
+    }    
+
+    private static void operaciones(CCuenta cuenta1) {
+        double saldoActual;
         // Operaciones de ingreso y retiro
         cuenta1.retirar(2300);
         cuenta1.ingresar(685);
-
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es " + saldoActual);
     }    
